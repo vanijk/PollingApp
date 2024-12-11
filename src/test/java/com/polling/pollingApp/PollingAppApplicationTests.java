8d8f0768eb3aeb1a47bf34ac6cc0c81f49f2a4d7
vanijk <vani.jk@gmail.com>
2024-12-11 09:39:44 -0600

    initial commit

---
+++ src/test/java/com/polling/pollingApp/PollingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polling.pollingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
